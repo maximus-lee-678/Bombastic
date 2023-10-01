f7a1f65770eaef9b06f58368cc7c2d94c6e3ea38
maximus-lee-678 <46864503+maximus-lee-678@users.noreply.github.com>
2023-10-01 21:38:03 +0800

    Initial commit 2

---
+++ create_venv.bat
@@ -0,0 +1,3 @@
+pip3.8 install virtualenv
+virtualenv venv_fyp
+pause
---
+++ execute.bat
@@ -0,0 +1,1 @@
+start cmd.exe /k "cd venv_fyp & Scripts\Activate & cd ..\FollowYourPose & ECHO accelerate launch txt2video.py --config="configs/pose_sample.yaml"  --skeleton_path="./pose_example/vis_ikun_pose2.mov""
