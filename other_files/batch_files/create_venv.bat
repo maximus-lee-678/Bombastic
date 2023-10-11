3f600f82917ffe326256d59102b044dc22709052
maximus-lee-678 <46864503+maximus-lee-678@users.noreply.github.com>
2023-10-11 23:09:05 +0800

    Changed local notebook version
    Downgrade of version to support jupyter_contrib_nbextensions for QOL features like time elapsed and collapsible headers
    Also fixed requirement issue for bitsandbytes to resolve MMPose error

--- other_files/batch_files/create_venv.bat
+++ other_files/batch_files/create_venv.bat
@@ -10,6 +10,17 @@ call Scripts\Activate
 cd ..
 python -m pip install ipykernel
 python -m pip install jupyter
+:: downgrade notebook to fix ModuleNotFoundError: No module named 'notebook.base' when installing nbextensions
+python -m pip install --upgrade notebook==6.4.12
+:: downgrade traitlets to fix  ModuleNotFoundError: No module named 'jupyter_server.contents' when launching server
+python -m pip install --upgrade traitlets==5.9.0
+python -m pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --sys-prefix
+python -m pip install jupyter_nbextensions_configurator
+jupyter nbextensions_configurator enable --sys-prefix
+:: themes: chesterish,grade3,gruvboxd,gruvboxl,monokai,oceans16,onedork,solarizedd,solarizedl
+python -m pip install jupyterthemes
+jt -t monokai
 python -m pip install -r ".\other_files\requirements\requirements_local_windows.txt"
 pause
 
