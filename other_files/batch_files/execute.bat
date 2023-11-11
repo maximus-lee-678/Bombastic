dacfbfb4cb217a6c52ca8e6e601849c80fd3c456
maximus-lee-678 <talk_to_max@hotmail.com>
2023-11-11 21:21:03 +0800

    Updated Windows notebook installation process

--- other_files/batch_files/execute.bat
+++
@@ -1,4 +0,0 @@
-:: move to root to run
-cd ../..
-
-start cmd.exe /k "cd venv_3104_team05 & Scripts\Activate & cd ..\FollowYourPose & ECHO accelerate launch txt2video.py --config="configs/pose_sample.yaml"  --skeleton_path="./pose_example/vis_ikun_pose2.mov""
---
+++ other_files/batch_files/pip_venv_quick_launch.bat
@@ -0,0 +1,4 @@
+:: move to root to run
+cd ../..
+
+start cmd.exe /k "cd venv_3104_team05 & Scripts\Activate
